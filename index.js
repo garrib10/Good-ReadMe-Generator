2702546a72ff8b54df49c848e08bb65829729db3
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-10-04 15:10:01 -0400

    index update

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-// array of questions for user
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require('fs');
+
+// Questions // 
+// Title // 
+// Description  // 
+// Table of Contents // 
+// Installation // 
+// Usage  // 
+// License  // 
+// Contributing  // 
+// Tests  // 
+
+// Questions for the Read Me Generator  // 
 const questions = [
 
 ];
