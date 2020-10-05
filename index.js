5b3beb9542cf8809cf4846db7652ab3a2aef72fe
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-10-04 22:17:43 -0400

    questions update

--- index.js
+++ index.js
@@ -14,17 +14,68 @@ const fs = require('fs');
 
 // Questions for the Read Me Generator  // 
 const questions = [
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "user",
+    },
+    {
+        type: "input",
+        message: "Waht is your email address?",
+        name: "email",
 
+    },
+    {
+        type: "input",
+        message: "What is the title of your Project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "What is the Project description?",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What are the Table of Contents for this project?",
+        name: "contents",
+    },
+    {
+        type: "input",
+        message: "What needs to be installed to make this project run properly?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "How will this application be used?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "What License will you be using?",
+        name: "license",
+    },
+    {
+        type: "input",
+        message: "Who are the contributors for this Project?",
+        name: "contributors",
+    },
+    {
+        type: "checkbox",
+        message: "Are you ready to run the test?",
+        choices: ["Yes", "No"],
+        name: "test",
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, utf8, function (err){
-  if (err){
-    throw err;
-  }
-   console.log("You have successfullly generated your ReadMe")
-});
+    fs.writeFile(fileName, data, utf8, function (err) {
+        if (err) {
+            return err;
+        }
+        console.log("You have successfullly generated your ReadMe")
+    });
 }
 
 
