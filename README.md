82aa95299e51fc4aea039ebf0f33eabc32df51e3
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-10-06 16:37:27 -0400

    file update 1

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
-# dfdfdf
+# dfdf
   ![github license](https://img.shields.io/badge/license-MIT-blue.svg)
+  
+  ## Description
+  dfdf
 
   ## Table of Contents
   * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#usage)
+  * [Tests](#tests)
+  * [Contributors](#contributors)
+  * [Questions](#questions)
 
   ## Installation
-  fdfdf
+  dfd
+
+  ## Usage 
+  dfd
+
+  ## License
+  MIT
+  
+
+  ## Tests
+  undefined
+
+  ## Contributors
+  dffd
+
+  ## Questions
+  undefined
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,12 +2,36 @@
 function generateMarkdown(data) {
   return `# ${data.title}
   ![github license](https://img.shields.io/badge/license-${data.license}-blue.svg)
+  
+  ## Description
+  ${data.description}
 
   ## Table of Contents
   * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#usage)
+  * [Tests](#tests)
+  * [Contributors](#contributors)
+  * [Questions](#questions)
 
   ## Installation
   ${data.installation}
+
+  ## Usage 
+  ${data.usage}
+
+  ## License
+  ${data.license}
+  
+
+  ## Tests
+  ${data.tests}
+
+  ## Contributors
+  ${data.contributors}
+
+  ## Questions
+  ${data.questions}
 `;
 }
 
